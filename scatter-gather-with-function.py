42bf62c9079c87988f522c546891d531beb36055
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-04-18 16:00:55 +0100

    Add function to split data for scatter

--- scatter-gather-with-function.py
+++ scatter-gather-with-function.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 import numpy as np
+import sys
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -10,20 +11,32 @@ def process_data(data):
     data = data + rank
     return data
 
+def split_data_for_scatter(data: list, n_ranks: int) -> list:
+    """Split a list into a list of lists for MPI scattering"""
+    data_length = len(data)
+    quot, rem = divmod(data_length, n_ranks)
+
+    if quot == 0:
+        print("Error: Length of data list should be >= number of MPI ranks!")
+        sys.stdout.flush()
+        comm.Abort()
+
+    # determine the size of each sub-task
+    counts = [quot + 1 if n < rem else quot for n in range(n_ranks)]
+
+    # determine the starting and ending indices of each sub-task
+    starts = [sum(counts[:n]) for n in range(n_ranks)]
+    ends = [sum(counts[:n + 1]) for n in range(n_ranks)]
+
+    # converts data into a list of arrays
+    scatter_data = [data[starts[n]:ends[n]] for n in range(n_ranks)]
+    return scatter_data
+
 if rank == 0:
     data = np.arange(5)
     data = np.tile(data, (10, 1))
 
-    # determine the size of each sub-task
-    ave, res = divmod(data.shape[0], nprocs)
-    counts = [ave + 1 if p < res else ave for p in range(nprocs)]
-
-    # determine the starting and ending indices of each sub-task
-    starts = [sum(counts[:p]) for p in range(nprocs)]
-    ends = [sum(counts[:p+1]) for p in range(nprocs)]
-
-    # converts data into a list of arrays
-    data = [data[starts[p]:ends[p]] for p in range(nprocs)]
+    data = split_data_for_scatter(data, nprocs)
 else:
     data = None
 
